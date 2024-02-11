b9613e763ca861a6bb9638812dd421a6ed797083
valentin <vb.brebionpro@gmail.com>
2024-02-11 23:18:35 +0100

    display username when connexion

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,21 @@
 <script setup>
+const username = JSON.parse(localStorage.getItem('user')).username;
 </script>
 
 <template>
   <main>
-    Hello 
+    <span class="welcome">Hello <h2 class="customUsername">{{ username }}</h2></span>
   </main>
 </template>
+<style scoped>
+.customUsername {
+  font-size: 1rem;
+  font-weight: bold;
+  margin-left: 1rem;
+}
+.welcome {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+</style>
